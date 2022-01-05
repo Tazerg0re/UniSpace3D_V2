ad5aaaf076f8f7e68b98a7206d49adddb7abfd55
Tazerg0re <chr.knoff@gmail.com>
2022-01-05 11:07:29 +0100

    Skripte angepasst um Safemode zu vermeiden

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,24 +5,24 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    privat Image HealthBar;
+   // private Image HealthBar;
     public float CurrentHealth;
     private float MaxHealth = 100f;
-    PlayerController_Script Player;
+    //PlayerController_Script Player;
 
 
     private void Start()
     {
         //Bitte das Script vom Player in Zeile 18 fügen,
         //da akteull falscher Name
-        HealthBar = GetComponent<Image>();
-        Player = FindObjectOfType<PlayerController_Script>();
+       // HealthBar = GetComponent<Image>();
+        //Player = FindObjectOfType<PlayerController_Script>();
     }
 
     
     private void Update()
     {
-        CurrentHealth = Player.Health;
-        HealthBar.fillAmount = CurrentHealth / MaxHealth;
+        //CurrentHealth = Player.Health;
+       // HealthBar.fillAmount = CurrentHealth / MaxHealth;
     }
 }
